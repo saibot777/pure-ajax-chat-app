e179f0622bf1e11337b4851650b4567392f53fd7
Stefan <stefantrajkovic777@gmail.com>
2016-11-08 15:28:46 +0100

    database created, server php file created and connected to database

--- server.php
+++ server.php
@@ -1,6 +1,33 @@
 <?php 
-	$data = $_GET;
-	$user_text = $_GET['user_text'];
-	$response = strtoupper( $user_text );
-	echo $response;
+
+	// Fetch Value
+	$data = $_REQUEST;
+	$last_displayed_chat_id = $data['last_displayed_chat_id'];
+
+	// Connect to MySQL Database
+	$con = mysqli_connect("localhost", "root", "", "ajax_chat");
+
+	$select = "SELECT *
+				FROM chats 
+				WHERE chat_id > '".last_displayed_chat_id."'
+			";
+	$result = mysqli_query( $con, $select );
+
+	$arr = array();
+	$row_count = mysqli_num_rows( $result );
+
+	if ($row_count > 0) {
+
+		while( $row = mysqli_fetch_array( $result ) ) {
+			array_push( $arr, $row );
+		}
+
+	}
+
+	// Close MySQL Connection
+	mysqli_close( $con );
+
+	// Return the response as JSON
+	echo json_encode( $arr );
+
  ?>
